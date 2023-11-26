161b4ccdec7b80d25742deadd3bafe25afb7c571
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-11-26 20:57:42 +0530

    Task 1 advance features complete.

--- src/Task_1/Number_Game.java
+++ src/Task_1/Number_Game.java
@@ -9,13 +9,15 @@ public class Number_Game {
     public static void main(String[] args) {
         int randNum = getRandom();
         int guessNum;
+        boolean got_guess = false;
 
         Scanner getInt = new Scanner(System.in);
 
+        System.out.println("#################### Welcome to the number guessing game ####################");
+
         for (int i = 0; i < 5; i++) {
-            System.out.println("\nRound "+i);
-            System.out.println("The number: "+randNum);
-            System.out.print("Guess the random number: ");
+            System.out.printf("\nRound %d",i+1);
+            System.out.print("\nGuess the random number: ");
             guessNum = getInt.nextInt();
 
             if (guessNum > randNum){
@@ -24,8 +26,13 @@ public class Number_Game {
                 System.out.println("Too low, Try Again.");
             } else {
                 System.out.println("Congratulation you have guessed correctly.");
+                System.out.printf("Your Score: %d/5",5-i);
+                got_guess = true;
                 break;
             }
+        }
+        if (!got_guess){
+            System.out.println("\nThe random number was "+randNum);
         }
     }
 }
